b5f17d5a82e5913a80455bf0fd3aaa55bb29c23a
Kshitij Prabhu <kshitij.prabhu@somaiya.edu>
2021-03-02 19:34:09 +0530

    Update Language

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(middleware.logger);
 
 
 app.get('/about',middleware.requireAuthentication, function(req,res){
-    res.send('About Me!');
+    res.send('About Me');
 });
 
 app.use(express.static(__dirname + '/public'));
