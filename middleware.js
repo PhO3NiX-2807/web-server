ebfcee7c3fcc6a824d8552927be392bcd02cb326
Kshitij Prabhu <kshitij.prabhu@somaiya.edu>
2021-03-02 19:43:35 +0530

    Refactor MiddleWare

---
+++ middleware.js
@@ -0,0 +1,12 @@
+middleware = {
+    requireAuthentication : function(req,res,next){
+        console.log('Private Route Hit');
+        next();
+    },
+    logger: function(req,res,next){
+        console.log('Request:'+ new Date().toString() + ' ' +  req.method + ' ' + req.originalUrl  );
+        next();
+    }
+};
+
+module.exports = middleware
--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express= require('express');
 var app= express();
+var middleware = require('./middleware.js')
 
-middleware = {
-    requireAuthentication : function(req,res,next){
-        console.log('Private Route Hit');
-        next();
-    },
-    logger: function(req,res,next){
-        console.log('Request:'+ new Date().toString() + ' ' +  req.method + ' ' + req.originalUrl  );
-        next();
-    }
-};
 app.use(middleware.logger);
 
 
